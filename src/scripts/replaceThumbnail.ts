7bb6c6949a40e4f93078c35e2ebb013680a80930
Daisuke Kusatake <dosaisen@gmail.com>
2023-04-06 23:26:31 +0900

    サムネイルの無限スクロールによる新規生成に対応

--- src/scripts/replaceThumbnail.ts
+++ src/scripts/replaceThumbnail.ts
@@ -1,5 +1,13 @@
 import image from '../assets/ninja_bunshin.png';
 const url = chrome.runtime.getURL(image)
 
-const nodeList = document.querySelectorAll<HTMLImageElement>('yt-image > img')
-nodeList.forEach(node => node.src = url)
+const config = { childList: true, subtree: true }
+
+const replace = () => {
+  const nodeList = document.querySelectorAll<HTMLImageElement>('yt-image > img')
+  nodeList.forEach(node => node.src = url)
+}
+
+const observer = new MutationObserver(replace)
+
+observer.observe(document.body, config)
