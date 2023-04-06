d9a3c0ab24ed0bb3203853bc2493156a2da75a76
Daisuke Kusatake <dosaisen@gmail.com>
2023-04-07 00:30:53 +0900

    プロフィール画像が非表示になるクラスを削除する処理を追加した

--- src/scripts/replaceThumbnail.ts
+++ src/scripts/replaceThumbnail.ts
@@ -1,11 +1,19 @@
-import image from '../assets/ninja_bunshin.png';
-const url = chrome.runtime.getURL(image)
+import thumbnail from '../assets/ninja_bunshin.png';
+import profile from '../assets/ninja_kabe.png';
+const thumbnailUrl = chrome.runtime.getURL(thumbnail)
+const profileUrl = chrome.runtime.getURL(profile)
 
 const config = { childList: true, subtree: true }
 
 const replace = () => {
-  const nodeList = document.querySelectorAll<HTMLImageElement>('yt-image > img')
-  nodeList.forEach(node => node.src = url)
+  const thumbnailImgNodeList = document.querySelectorAll<HTMLImageElement>('yt-image > img')
+  thumbnailImgNodeList.forEach(node => node.src = thumbnailUrl)
+
+  const profileImgNodeList = document.querySelectorAll<HTMLImageElement>('yt-img-shadow > img')
+  profileImgNodeList.forEach(node => node.src = profileUrl)
+
+  const profileNodeList = document.querySelectorAll<HTMLImageElement>('yt-img-shadow')
+  profileNodeList.forEach(node => node.classList.remove('empty'))
 }
 
 const observer = new MutationObserver(replace)
